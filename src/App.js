1cf2993e7c47a9495f652a024b88aa0a228d8848
doulath123 <96289292+doulath123@users.noreply.github.com>
2022-03-14 11:04:56 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import {AddHouse} from "../Components/AddHouse/AddHouse.jsx"
+
+function App() {
+  return (
+    <div className="App">
+     <AddHouse/>
+    </div>
+  );
+}
+
+export default App;
